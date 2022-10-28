1bf8584bebd1176d448503e7d99fd9683ba62cad
Hamish Friedlander <hafriedlander@gmail.com>
2022-10-29 12:14:39 +1300

    Allow HF_HOME to be used in config, and delay to show error message before exiting

--- scripts/run.py
+++ scripts/run.py
@@ -1,7 +1,6 @@
 from os import path
 import subprocess, os, shutil, sys
 from dotenv import load_dotenv
-from sdgrpcserver import server
 
 def main():
     base=path.dirname(path.dirname(__file__))
@@ -12,12 +11,19 @@ def main():
     
     hfToken = os.environ.get('HF_API_TOKEN', '')
     if not hfToken or hfToken == '{your huggingface token}':
-        print("You need to register an account on HuggingFace, create an API token, and save it into the 'config' file in this directory")
+        print("ERROR: You need to register an account on HuggingFace, create an API token, and save it into the 'config' file in this directory")
+        input("Press enter to exit")
+        sys.exit(-1)
+    if hfToken[0] == "{":
+        print("ERROR: Don't wrap your token with {} brackets.")
+        input("Press enter to exit")
         sys.exit(-1)
 
     # Run server
 
     os.environ["SD_HTTP_FILE_ROOT"] = path.join(base, "idea2art")
+
+    from sdgrpcserver import server
     server.main()
     
 if __name__ == "__main__":
