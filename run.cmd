496228b5159d303d110ee8a048998f9519f3be01
Hamish Friedlander <hafriedlander@gmail.com>
2022-11-21 21:32:19 +1300

    Use conda instead of micromamba if its available

--- run.cmd
+++ run.cmd
@@ -1,3 +1,13 @@
 @echo off
-start .\env\micromamba.exe -r env -n sd-grpc-server run python ./scripts/run.py
+
+rem Detect conda, or use micromamba if it doesn't exist
+if [%CONDA_EXE%] EQU [] (
+    set MAMBA_ROOT_PREFIX=%~dp0env
+    set SDCONDA_EXE=%~dp0env\micromamba.exe
+) else (
+    set SDCONDA_EXE=%CONDA_EXE%
+)
+
+rem Use python to handle the rest of the run process
+"%SDCONDA_EXE%" -n sd-grpc-server run python ./scripts/run.py
 
