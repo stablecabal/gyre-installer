32f2dc0a812390b04ef30511b3ca88a42e8dcd5d
Hamish Friedlander <hafriedlander@gmail.com>
2023-02-16 23:46:09 +1300

    Add some basic pre-install checked (can find a decent Nvidia card, has enough drive space for models)

--- scripts/install_or_update.py
+++ scripts/install_or_update.py
@@ -1,5 +1,75 @@
 from os import path
-import os, subprocess, shutil, re, sys
+import os, subprocess, shutil, re, sys, time
+
+from lib.colorama import  just_fix_windows_console, Fore, Style
+just_fix_windows_console()
+
+from lib import pynvml
+
+GB = 1024 * 1024 * 1024
+
+def check_environment(hf_cache_home):
+    pynvml.nvmlInit()
+
+    has_warning = False
+
+    def error(str):
+        print(Fore.RED, "\n----Error--------------------------")
+        print(str)
+        print("-----------------------------------", Style.RESET_ALL)
+        sys.exit(-1)
+
+    def warning(str):
+        nonlocal has_warning
+        has_warning = True
+        print(Fore.YELLOW, "\n----Warning--------------------------")
+        print(str)
+        print("-------------------------------------", Style.RESET_ALL)
+
+    try:
+
+        # Find a pynvml card
+
+        count = 0
+        try:
+            count = pynvml.nvmlDeviceGetCount()
+        except pynvml.NVMLError as error:
+            print(error)
+
+        if count == 0:
+            error("Could not find any Nvidia cards. Gyre requires a recent NVidia card.")
+            sys.exit(-1)
+
+        # Check max ram and arch
+
+        max_ram = 0
+        max_arch = 0
+        for i in range(count):
+            handle = pynvml.nvmlDeviceGetHandleByIndex(i)
+            info = pynvml.nvmlDeviceGetMemoryInfo(handle)
+            max_ram = max(max_ram, info.total)
+            max_arch = max(max_arch, pynvml.nvmlDeviceGetArchitecture(handle))
+
+        if max_ram < 3.9*GB:
+            warning("Could not find an NVidia card with at least 4GB RAM.\nYou can still try and use Gyre, but some functions may fail.")
+
+        if max_arch < pynvml.NVML_DEVICE_ARCH_PASCAL:
+            warning("Could not find an Nvidia card with at least the Pascal architecture (10xx).\nGyre probably won't work.")
+        elif max_arch < pynvml.NVML_DEVICE_ARCH_TURING:
+            warning("Could not find an Nvidia card with at least the Turing architecture (20xx).\nGyre should work, but you might run into some issues.")
+
+        # Check disk space
+        os.makedirs(hf_cache_home, exist_ok=True)
+        usage = shutil.disk_usage(hf_cache_home)
+
+        if usage.free < 40 * GB:
+            warning(f"Less than 40GB free for Huggingface Cache at {hf_cache_home}.\nIf you haven't downloaded the models yet, you will need to free more drive space before running run.cmd.")
+
+    finally:
+        pynvml.nvmlShutdown()
+
+    return has_warning
+
 
 def main():
     base=path.dirname(path.dirname(path.abspath(__file__)))
@@ -14,14 +84,30 @@ def main():
     repo = os.environ.get("AIO_REPO", "https://github.com/stablecabal/gyre-installer.git")
     branch = os.environ.get("AIO_BRANCH", "main") 
 
+    # From huggingface_hub/constants.py
+    default_home = os.path.join(os.path.expanduser("~"), ".cache")
+    hf_cache_home = os.path.expanduser(
+        os.getenv(
+            "HF_HOME",
+            os.path.join(os.getenv("XDG_CACHE_HOME", default_home), "huggingface"),
+        )
+    )
+
     # We can't rely on dotenv existing yet, stdlib only
     with open(path.join(base, "config"), "r") as config_file:
         for line in config_file:
-            repo_match = re.match(r'\s*AIO_REPO=(\S+)', line)
+            repo_match = re.match(r'\s*AIO_REPO\s*=\s*(\S+)', line)
             branch_match = re.match(r'\s*AIO_BRANCH\s*=\s*(\S+)', line)
+            home_match = re.match(r'\s*HF_HOME\s*=\s*(\S+)', line)
 
             if repo_match: repo=repo_match.group(1)
             if branch_match: branch=branch_match.group(1)
+            if home_match: hf_cache_home=os.path.abspath(home_match.group(1))
+
+    # Run a basic environment check before doing anything
+    if check_environment(hf_cache_home):
+        print("(Continuing in 5 seconds....)", flush=True)
+        time.sleep(5)
 
     # Update this repo, and any submodules
 
---
+++ scripts/lib/README.md
@@ -0,0 +1,2 @@
+Contains code from https://pypi.org/project/nvidia-ml-py/, distributed under the BSD License
+Contains code from https://pypi.org/project/colorama/, distributed under the BSD License
